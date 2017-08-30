d904e2428ace99f536d41c32b1ddfbd4076f7c20
ahaha0807 <b5331@oic.jp>
2017-08-31 02:32:31 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000, function () {
+    console.log("server is working!");
+})
+
+app.get('/', function (request, response, next) {
+    response.send("Hello World")
+})
+
+app.use(express.static('public'))
+
+app.get('/html', function (request, response, next) {
+    response.sendFile('hello.html', {
+        root: __dirname + '/public/'
+    })
+})
+
+app.get('/api', function (request, response, next) {
+    response.json({
+        "message": "Hello World"
+    })
+})
+
+// Express（われわれ）はかしこいので、設定とかルーティングとかをどんどん下に追記していっても大丈夫なのです
+// ただ、クッソ読みにくいので、下のような感じでやってることごとにまとめましょう
+
+/**
+const express = require('express')
+const app = express()
+
+app.listen(3000, function () {
+    console.log("server is working!");
+})
+app.use(express.static('public'))
+
+app.get('/', function (request, response, next) {
+    response.send("Hello World")
+})
+
+app.get('/html', function (request, response, next) {
+    response.sendFile('hello.html', {
+        root: __dirname + '/public/'
+    })
+})
+
+app.get('/api', function (request, response, next) {
+    response.json({"message": "Hello World"})
+})
+ */
